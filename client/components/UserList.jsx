4650c7a6c63fe0bfab980c14b8789c1860a33b52
diegoA-66 <abreugdiego@gmail.com>
2021-03-04 23:11:15 -0500

    false changes

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -4,15 +4,15 @@ import User from './User.jsx';
 export default ({ users }) => (
   <div className='user-list'>
     <p className='lead'>
-      Here's a list of some {users.length} completely fictitious people:
+      Here's a list of hellow {users.length} completely fictitious people:
     </p>
 
-    <table className='table'>
+    {/* <table className='table'>
       <tbody>
         {users.map((u) => (
           <User {...u} key={u.id} />
         ))}
       </tbody>
-    </table>
+    </table> */}
   </div>
 );
